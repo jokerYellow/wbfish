e7753aa0360c3542a42958f8ca590d39bfb28891
ppp <piase1910@163.com>
2024-07-04 07:16:32 +0800

    feat: Add timezone support to currentDate function

--- app/utils.tsx
+++ app/utils.tsx
@@ -1,7 +1,11 @@
 import dayjs from "dayjs";
+import timezone from 'dayjs/plugin/timezone' // 导入插件
+import utc from 'dayjs/plugin/utc' // 导入插件
 
 function currentDate() {
-    return dayjs().format('YYYYMMDD');
+    dayjs.extend(timezone)
+    dayjs.extend(utc)
+    return dayjs().tz("Asia/Shanghai").format("YYYYMMDD");
 }
 
 export const defaultDate = currentDate;
