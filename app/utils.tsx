20d49644e6062695616931c23dfc340f270d9cbb
ppp <piase1910@163.com>
2024-07-24 23:55:42 +0800

    refactor: Update currentDate function to support timezone and optimize code formatting

--- app/utils.tsx
+++ app/utils.tsx
@@ -1,27 +1,29 @@
 import dayjs from "dayjs";
-import timezone from 'dayjs/plugin/timezone' // 导入插件
-import utc from 'dayjs/plugin/utc' // 导入插件
+import timezone from "dayjs/plugin/timezone"; // 导入插件
+import utc from "dayjs/plugin/utc"; // 导入插件
 
 function currentDate() {
-    dayjs.extend(timezone)
-    dayjs.extend(utc)
-    return dayjs().tz("Asia/Shanghai").format("YYYYMMDD");
+  dayjs.extend(timezone);
+  dayjs.extend(utc);
+  return dayjs().tz("Asia/Shanghai").format("YYYYMMDD");
 }
 
-
 export function formatTimeAgo(date: Date): string {
-    const now = dayjs().tz("Asia/Shanghai");
-    const postDate = dayjs(date);
-    const diff = now.diff(postDate, "minute");
+  dayjs.extend(timezone);
+  dayjs.extend(utc);
+  dayjs().tz("Asia/Shanghai");
+  const now = dayjs().utcOffset(8);
+  const postDate = dayjs(date);
+  const diff = now.diff(postDate, "minute");
 
-    if (diff < 60) {
-      return `${diff} minutes ago`;
-    } else if (diff < 1440) {
-      const hours = Math.floor(diff / 60);
-      return `${hours} hours ago`;
-    } else {
-      return postDate.format("YYYY-MM-DD HH:mm");
-    }
+  if (diff < 60) {
+    return `${diff} minutes ago ${now.format("YYYY-MM-DD HH:mm")}`;
+  } else if (diff < 1440) {
+    const hours = Math.floor(diff / 60);
+    return `${hours} hours ago ${now.format("YYYY-MM-DD HH:mm")}`;
+  } else {
+    return postDate.format("YYYY-MM-DD HH:mm");
   }
+}
 
 export const defaultDate = currentDate;
